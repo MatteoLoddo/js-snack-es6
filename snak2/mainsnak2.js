07254cd8005c152c2af5511bc387f065a9da6c79
MatteoLoddo <loddomatteo96@gmail.com>
2022-06-07 14:31:44 +0200

    push colmpleted first milestone

--- snak2/mainsnak2.js
+++ snak2/mainsnak2.js
@@ -1,40 +1,48 @@
 
 // creo un arrray con oggetti
-const squadre = [
+const listaSquadre = [
     {
         nome: "Real Madrid",
+        punti: 0,
+        falli: 0,
     },
     {
         nome: "Milan",
+        punti: 0,
+        falli: 0,
     },
     {
         nome: "Inter",
+        punti: 0,
+        falli: 0,
     },
     {
         nome: "Juventus",
+        punti: 0,
+        falli: 0,
     }
 ]
 
-console.log(squadre);
+console.log(listaSquadre);
 // creo un secondo array dove pushare gli oggetti con nuove KEY
 const squadreConInfo = []
 
 // creo il ciclo per ciclare gli elementi nell array
-for(let i=0; i<squadre.length; i++){
+for(let i=0; i<listaSquadre.length; i++){
     // creo due variabili che saranno le nuove KEY 
-    const goal = Math.floor(Math.random()*100);
-    const falli = Math.floor(Math.random()*100);
-
-
-    // console.log(squadre[i], goal , falli);
-    // pusho ogni oggetto aggiornandolo con la KEY che mi seve
-    squadreConInfo.push({
-        ...squadre[i],
-        falli,
-    })
-
+    listaSquadre[i].punti = Math.floor(Math.random()*100);
+    listaSquadre[i].falli = Math.floor(Math.random()*100);
 
 
 }
 
+for(let i=0; i<listaSquadre.length; i++){
+const {nome,falli} = listaSquadre[i]
+}
+
 console.log(squadreConInfo);
+
+
+for(let key in listaSquadre){
+    console.log(listaSquadre[key]);
+}
